79201aa36987176d00800fcbc9fc8e1937f34222
Teun Zengerink <t.zengerink@gmail.com>
2015-12-03 16:58:42 +0100

    introduced if block

--- clock.py
+++ clock.py
@@ -7,26 +7,27 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from functools import partial
 
 
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 def ping(url):
     def fn():
-        log.info('Pinging {}'.format(url))
+        LOG.info('Pinging {}'.format(url))
         return requests.get(url)
     return fn
 
 
-scheduler = BlockingScheduler()
-scheduler.add_job(
-    ping('https://challenge-backend.herokuapp.com/ping'),
-    trigger='cron',
-    minute='*/20',
-    hour='8-21')
-scheduler.add_job(
-    ping('https://groceries-api.herokuapp.com/status'),
-    trigger='cron',
-    minute='*/20',
-    hour='7-22')
-scheduler.start()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        ping('https://challenge-backend.herokuapp.com/ping'),
+        trigger='cron',
+        minute='*/20',
+        hour='8-21')
+    scheduler.add_job(
+        ping('https://groceries-api.herokuapp.com/status'),
+        trigger='cron',
+        minute='*/20',
+        hour='7-22')
+    scheduler.start()
